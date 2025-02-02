a878e6aa5568f83ca1b569a39a20bfa0e83d1529
WaseemS02 <waseemssadi@gmail.com>
2025-02-02 14:20:28 +0200

    Initial commit for backend

---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { bookVisit, cancelBooking, createUser, getAllBookings } from "../controllers/userCntrl.js";
+
+const router = express.Router();
+
+router.post("/register", createUser);
+router.post("/bookVisit/:id",bookVisit);
+router.get("/allBookings", getAllBookings)
+router.post("/removeBooking/:id", cancelBooking)
+
+export {router as userRoute}
