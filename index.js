a9865e720d218df0f427f126dfcafb12b4d33723
WaseemS02 <waseemssadi@gmail.com>
2025-02-02 16:32:18 +0200

    Added default route

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { userRoute } from "./routes/userRoute.js";
 import { residencyRoute } from "./routes/residencyRoute.js";
+
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+app.use("/api/user", userRoute);
+app.use("/api/residency", residencyRoute);
+
+// ✅ Default route for the root `/`
+app.get("/", (req, res) => {
+  res.send("Backend is Running 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
-
-app.use("/api/user", userRoute);
-app.use("/api/residency", residencyRoute)
